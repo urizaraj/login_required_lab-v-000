94e125d6dcbd273d51b74131f7b477f90af0f874
Alexander Urizar <urizaraj@gmail.com>
2018-03-19 15:30:17 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'sessions#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  post '/login', to: 'sessions#destroy'
+  post '/logout', to: 'sessions#destroy'
 
   get '/secrets', to: 'secrets#show'
 end
